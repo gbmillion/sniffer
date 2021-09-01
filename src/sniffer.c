5f60ea39b67101ea1724dac43c630718b5a7c6fe
gbmillion <gbmillion@bucs.fsw.edu>
2021-09-01 10:58:01 -0400

    packet sniffer complete

--- src/sniffer.c
+++ src/sniffer.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <pcap.h>
 #include <string.h>
-#define DEBUG ;
+
+//#define DEBUG ;
 int main(int argc, char *argv[])
 {
 	if (argc<2){
-		printf("./sniffer \"search phrase\" pcap filter rules\n ");
+		printf("./sniffer \"sniffing device\" \"search phrase\" pcap filter rules\n");
 		return 1;
 	}
 	//local vars
-	char filter [255], *dev, errbuf[PCAP_ERRBUF_SIZE];
+	char filter [255],*buff, dev[255], errbuf[PCAP_ERRBUF_SIZE];
 	int i=0;
 
 	pcap_t *handle;				// pcap handle
@@ -18,28 +19,38 @@ int main(int argc, char *argv[])
 	bpf_u_int32 net;		//  IP of our sniffing device
 	const u_char *packet;		// actual packet
 	struct pcap_pkthdr header;	// header from pcap
-	dev = pcap_lookupdev(errbuf);//look up default device
+
+	if(0 == strcmp( "",  argv[1] )){//if no device is specified use default
+		buff = pcap_lookupdev(errbuf);//look up default device
+	} else {
+		strcpy(  dev, argv[1] );
+		buff = dev;//use user defined interface
+	}
+
 	if (dev == NULL) {
 		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
 		return(2);
 	}
+#if defined DEBUG
+	printf("%s",dev);
+#endif
 
-	for(i=2;i<argc;i++){//convert argument to filter
+	for(i=3;i<argc;i++){//convert argument to filter
 		strcat( filter, argv[i] );
 		strcat( filter, " " );
 #if defined DEBUG
 		printf("%s\n",filter);
 #endif
 	}
-	printf("Using default device: %s\n", dev);
 
-	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);//open default device
+	printf("Using  device: %s\n", buff);
+	handle = pcap_open_live(buff, BUFSIZ, 1, 1000, errbuf);//open default device
 	if (handle == NULL) {
-		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+		fprintf(stderr, "Couldn't open device %s: %s\n", buff, errbuf);
 		return(2);
 	} else printf("Device opened.\n");
-	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {//get network info from dev
-		fprintf(stderr, "Can't get netmask for device %s\n", dev);
+	if (pcap_lookupnet(buff, &net, &mask, errbuf) == -1) {//get network info from dev
+		fprintf(stderr, "Can't get netmask for device %s\n", buff);
 		net = 0;
 		mask = 0;
 	}
@@ -50,8 +61,11 @@ int main(int argc, char *argv[])
 	char  results;
 	while (1){//loop while gtting packets
 		packet = pcap_next(handle, &header);
-		results = strstr(   (char *)packet,  argv[1]);
-		if (results) printf("[%s]\n", &results);
+#if defined DEBUG
+		printf("comparing: packet[%s]argv[%s]*\n",  packet,argv[2]);
+#endif
+		results = strstr(   (char *)packet,   (char *)argv[2]); // this does not work
+		if(results)printf("results[%s]\n",  &results);
 	}
 	pcap_close(handle);
 
